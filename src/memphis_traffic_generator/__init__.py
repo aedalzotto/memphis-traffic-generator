e86e4e94bd8571d285223e5c6ce79cde8436576e
Angelo Elias Dal Zotto <angelo.dalzotto@edu.pucrs.br>
2024-10-07 11:44:53 -0300

    Add management and extract single scenario options

--- src/memphis_traffic_generator/__init__.py
+++ src/memphis_traffic_generator/__init__.py
@@ -19,6 +19,7 @@ def memphis_tg():
     gen_ht_parser = subparsers.add_parser("generate-ht", help="Generate testcases and scenarios for HT-based anomaly")
     gen_ht_parser.add_argument("APPLICATION", help="Application name to generate")
     gen_ht_parser.add_argument("-o", "--output", help="Path to output base folder", default=".")
+    gen_ht_parser.add_argument("-m", "--with-management", help="Add ODA detection tasks", action="store_true", default=False)
     
     build_parser = subparsers.add_parser("build", help="Generate testcase and scenarios")
     build_parser.add_argument("TESTCASE", help="Testcase file")
@@ -43,6 +44,12 @@ def memphis_tg():
     ext_ht_parser.add_argument("-l", "--lower",  help="Lower bound", default=None)
     ext_ht_parser.add_argument("-u", "--upper",  help="Upper bound", default=None)
 
+    ext_single_parser = subparsers.add_parser("extract-single", help="Extract dataset from single scenario with no HT")
+    ext_single_parser.add_argument("SCENARIO", help="Normal scenario path")
+    ext_single_parser.add_argument("-o", "--output", help="Output file", default=None)
+    ext_single_parser.add_argument("-l", "--lower",  help="Lower bound", default=None)
+    ext_single_parser.add_argument("-u", "--upper",  help="Upper bound", default=None)
+
     args = parser.parse_args()
     if args.option == "generate-app" or args.option == "generate-ht":
         MEMPHIS_V_PATH = getenv(ENV_MEMPHIS_V_PATH)
@@ -52,7 +59,7 @@ def memphis_tg():
         if args.option == "generate-app":
             generator = Generator(MEMPHIS_V_PATH, args.APPLICATION, mal_msg_size=args.msize)
         else:
-            generator = Generator(MEMPHIS_V_PATH, args.APPLICATION, trojan=True)
+            generator = Generator(MEMPHIS_V_PATH, args.APPLICATION, trojan=True, oda=args.with_management)
 
         generator.write(args.output)
     elif args.option == "build":
@@ -66,6 +73,9 @@ def memphis_tg():
         extractor.extract(args.output)
     elif args.option == "extract-ht":
         extractor = Extractor(args.TESTCASE, args.lower, args.upper, args.TESTCASE_M)
+        extractor.extract(args.output)
+    elif args.option == "extract-single":
+        extractor = Extractor(None, args.lower, args.upper, None, args.SCENARIO)
         extractor.extract(args.output)
     else:
         parser.print_usage()
