4186b7ff71a603c12946b247cca7faef3526dfa1
Angelo Elias Dal Zotto <angelodalzotto97@gmail.com>
2026-01-17 13:43:14 -0300

    update filtering function

--- src/memphis_traffic_generator/tools.py
+++ src/memphis_traffic_generator/tools.py
@@ -3,16 +3,29 @@ from os import listdir
 def __index_of(scenario):
     return int(scenario.split("/")[-1].split(".")[0].split("_")[1])
 
-def get_scenarios(testcase, no_base=False, with_ht=False, with_mapp=False):
-    scenarios = sorted(["{}/{}".format(testcase, scenario) for scenario in listdir(testcase) if scenario.startswith("sc_")])
+def get_scenarios(base, with_base=False, with_test=False, with_ht=False, with_mapp=False, with_fp=False, keep_yaml=False):
+    scenarios = sorted(["{}/{}".format(base, scenario) for scenario in listdir(base) if scenario.startswith("sc_")])
 
-    if no_base:
-        scenarios = list(filter(lambda scenario: (scenario.endswith("_ht") or scenario.endswith("_mapp")), scenarios))
-        
+    # Keep everything not base        
+    if not with_base:
+        scenarios = list(filter(lambda scenario: scenario.endswith(("_ht{}".format(".yaml" if keep_yaml else ""), "_rtd{}".format(".yaml" if keep_yaml else ""), "_fp{}".format(".yaml" if keep_yaml else ""))), scenarios))
+
+    # Remove everything test-related
+    if not with_test:
+        scenarios = list(filter(lambda scenario: not scenario.endswith(("_normal_rtd{}".format(".yaml" if keep_yaml else ""), "_ht{}".format(".yaml" if keep_yaml else ""))), scenarios))
+
+    # Remove everything HT-related
     if not with_ht:
-        scenarios = list(filter(lambda scenario: not scenario.endswith("_ht"), scenarios))
+        scenarios = list(filter(lambda scenario: not scenario.endswith("ht_rtd{}".format(".yaml" if keep_yaml else "")), scenarios))
 
+    # Remove everything with Mapp
     if not with_mapp:
-        scenarios = list(filter(lambda scenario: not scenario.endswith("_mapp"), scenarios))
+        scenarios = list(filter(lambda scenario: not scenario.endswith("_mapp_rtd{}".format(".yaml" if keep_yaml else "")), scenarios))
+
+    # Remove everything with FP
+    if not with_fp:
+        scenarios = list(filter(lambda scenario: not scenario.endswith("_fp{}".format(".yaml" if keep_yaml else "")), scenarios))
+    else:
+        scenarios = list(filter(lambda scenario: not scenario.endswith("rtd_fp{}".format(".yaml" if keep_yaml else "")), scenarios))
 
     return scenarios
